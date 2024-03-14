1851d7a1c1f9fde3cf5cf36edfab395c02e28247
Vũ Việt Hưng <vuviethungnbee@gmail.com>
2024-03-14 15:40:12 +0700

    Adding api_view

--- import_data/api/views.py
+++ import_data/api/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
 from django.http import *
 from rest_framework.response import Response
 from rest_framework import status
 import pandas as pd
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
+from rest_framework.decorators import api_view
 
 
 # Create your views here.
